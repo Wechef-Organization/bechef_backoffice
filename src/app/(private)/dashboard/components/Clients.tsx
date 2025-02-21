9ab75b596106dd52d84a378c4f43aabe60e67b2f
m7-startpn <mauricio@gopn.ai>
2025-02-21 13:17:12 -0300

    add: filter in header table in Ranking, Recipies and Clients Components

--- src/app/(private)/dashboard/components/Clients.tsx
+++ src/app/(private)/dashboard/components/Clients.tsx
@@ -4,11 +4,19 @@ import { useEffect, useState } from "react"
 
 import InputSearch from "@/components/InputSearch"
 import { useDashboard } from "@/context/DashboardContext"
+import { ClientItem } from "@/interfaces/dashboardContext"
 import { getClients } from "@/utils/dashboardReq/clients"
 
 const Clients = () => {
     const { clientsList, setClientsList } = useDashboard()
     const [value, setValue] = useState("")
+    const [sortOrder, setSortOrder] = useState<{
+        field: "id" | "sales_number" | "total";
+        direction: "asc" | "desc";
+    }>({
+        field: "id",
+        direction: "asc",
+    })
 
     useEffect(() => {
         const featchInfluencers = async () => {
@@ -18,11 +26,40 @@ const Clients = () => {
         featchInfluencers()
     }, [])
 
+    const handleSort = (field: "id" | "sales_number" | "total") => {
+        setSortOrder((prevState) => {
+            if (prevState.field === field) {
+
+                return {
+                    field: field,
+                    direction: prevState.direction === "asc" ? "desc" : "asc",
+                };
+            }
+            return {
+                field: field,
+                direction: "asc",
+            };
+        });
+    };
+
+
     const filteredPatients = value
-        ? clientsList.filter((item) =>
+        ? clientsList.filter((item: ClientItem) =>
             item.name.toLowerCase().includes(value.toLowerCase())
         )
         : clientsList;
+
+
+    const sortedPatients = [...filteredPatients].sort((a, b) => {
+        const aValue = a[sortOrder.field];
+        const bValue = b[sortOrder.field];
+
+        if (sortOrder.direction === "asc") {
+            return aValue > bValue ? 1 : aValue < bValue ? -1 : 0;
+        } else {
+            return aValue < bValue ? 1 : aValue > bValue ? -1 : 0;
+        }
+    });
 
     return (
         <div className="w-[49%] border border-border rounded-lg">
@@ -31,26 +68,24 @@ const Clients = () => {
                 <InputSearch value={value} setValue={setValue} />
             </div>
             <div className="w-full px-4 py-2 flex flex-row items-center bg-[#F9F9FC]">
-                <div className="w-[10%] flex flex-row gap-2 ">
+                <div className="w-[10%] flex flex-row gap-2 cursor-pointer" onClick={() => handleSort("id")}>
                     <p className="text-xs font-medium text-grey2">ID</p>
                     <Image alt={"Seta para baixo"} src={"/images/Dashboard/arrowDown.svg"} width={8} height={4} />
                 </div>
                 <div className="w-[50%] flex flex-row gap-2 ">
                     <p className="text-xs font-medium text-grey2">Nome do cliente</p>
                 </div>
-                <div className="w-[20%] flex flex-row gap-2 ">
+                <div className="w-[20%] flex flex-row gap-2 cursor-pointer" onClick={() => handleSort("sales_number")}>
                     <p className="text-xs font-medium text-grey2">N° vendas</p>
                     <Image alt={"Seta para baixo"} src={"/images/Dashboard/arrowDown.svg"} width={8} height={4} />
-
                 </div>
-                <div className="w-[20%] flex flex-row gap-2 ">
+                <div className="w-[20%] flex flex-row gap-2 cursor-pointer" onClick={() => handleSort("total")}>
                     <p className="text-xs font-medium text-grey2">Total</p>
                     <Image alt={"Seta para baixo"} src={"/images/Dashboard/arrowDown.svg"} width={8} height={4} />
-
                 </div>
             </div>
             <ul className="overflow-y-auto h-[330px]">
-                {filteredPatients.length > 0 && filteredPatients.map((elm) => (
+                {sortedPatients.length > 0 && sortedPatients.map((elm) => (
                     <div className="w-full px-4 py-2 flex flex-row items-center" key={elm.id}>
                         <div className="w-[10%] pl-2 flex flex-row gap-2">
                             <p className="text-xs font-medium text-grey2">{elm.id}</p>
--- src/app/(private)/dashboard/components/Recipies.tsx
+++ src/app/(private)/dashboard/components/Recipies.tsx
@@ -5,11 +5,19 @@ import { useEffect, useState } from "react"
 import InputSearch from "@/components/InputSearch"
 import UserImage from "@/components/UserImage"
 import { useDashboard } from "@/context/DashboardContext"
+import { RecipieItem } from "@/interfaces/dashboardContext"
 import { getRecipies } from "@/utils/dashboardReq/recipies"
 
 const Recipies = () => {
     const { recipiesList, setRecipiesList } = useDashboard()
     const [value, setValue] = useState("")
+    const [sortOrder, setSortOrder] = useState<{
+        field: "id" | "sales_number" | "total";
+        direction: "asc" | "desc";
+    }>({
+        field: "id",
+        direction: "asc",
+    })
 
     useEffect(() => {
         const featchInfluencers = async () => {
@@ -19,11 +27,40 @@ const Recipies = () => {
         featchInfluencers()
     }, [])
 
+    const handleSort = (field: "id" | "sales_number" | "total") => {
+        setSortOrder((prevState) => {
+            if (prevState.field === field) {
+
+                return {
+                    field: field,
+                    direction: prevState.direction === "asc" ? "desc" : "asc",
+                };
+            }
+            return {
+                field: field,
+                direction: "asc",
+            };
+        });
+    };
+
+
     const filteredPatients = value
-        ? recipiesList.filter((item) =>
+        ? recipiesList.filter((item: RecipieItem) =>
             item.description.toLowerCase().includes(value.toLowerCase())
         )
         : recipiesList;
+
+
+    const sortedPatients = [...filteredPatients].sort((a, b) => {
+        const aValue = a[sortOrder.field];
+        const bValue = b[sortOrder.field];
+
+        if (sortOrder.direction === "asc") {
+            return aValue > bValue ? 1 : aValue < bValue ? -1 : 0;
+        } else {
+            return aValue < bValue ? 1 : aValue > bValue ? -1 : 0;
+        }
+    });
 
     return (
         <div className="w-[49%] border border-border rounded-lg">
@@ -32,26 +69,24 @@ const Recipies = () => {
                 <InputSearch value={value} setValue={setValue} />
             </div>
             <div className="w-full px-4 py-2 flex flex-row items-center bg-[#F9F9FC]">
-                <div className="w-[10%] flex flex-row gap-2 ">
+                <div className="w-[10%] flex flex-row gap-2 cursor-pointer" onClick={() => handleSort("id")}>
                     <p className="text-xs font-medium text-grey2">ID</p>
                     <Image alt={"Seta para baixo"} src={"/images/Dashboard/arrowDown.svg"} width={8} height={4} />
                 </div>
                 <div className="w-[50%] flex flex-row gap-2 ">
                     <p className="text-xs font-medium text-grey2">Nome da receita</p>
                 </div>
-                <div className="w-[20%] flex flex-row gap-2 ">
+                <div className="w-[20%] flex flex-row gap-2 cursor-pointer" onClick={() => handleSort("sales_number")}>
                     <p className="text-xs font-medium text-grey2">N° vendas</p>
                     <Image alt={"Seta para baixo"} src={"/images/Dashboard/arrowDown.svg"} width={8} height={4} />
-
                 </div>
-                <div className="w-[20%] flex flex-row gap-2 ">
+                <div className="w-[20%] flex flex-row gap-2 cursor-pointer" onClick={() => handleSort("total")}>
                     <p className="text-xs font-medium text-grey2">Total</p>
                     <Image alt={"Seta para baixo"} src={"/images/Dashboard/arrowDown.svg"} width={8} height={4} />
-
                 </div>
             </div>
             <ul className="overflow-y-auto h-[330px]">
-                {filteredPatients.length > 0 && filteredPatients.map((elm) => (
+                {sortedPatients.length > 0 && sortedPatients.map((elm) => (
                     <div className="w-full px-4 py-2 flex flex-row items-center" key={elm.id}>
                         <div className="w-[10%] pl-2 flex flex-row gap-2">
                             <p className="text-xs font-medium text-grey2">{elm.id}</p>
